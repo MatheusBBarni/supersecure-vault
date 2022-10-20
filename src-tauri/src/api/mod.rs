b9cec936b2fad046c43b2f9afae24f6cddcef442
MatheusBBarni <brehm.matheus@hotmail.com>
2022-10-20 15:25:47 -0300

    chore: added prisma to the project and tried to add wit rspc

--- src-tauri/src/api/mod.rs
+++ src-tauri/src/api/mod.rs
@@ -1,21 +1,26 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::Arc};
 
 pub use rspc::{Config, RouterBuilder};
 
-#[derive(Clone)]
-pub struct Ctx {}
+use crate::prisma;
+
+// #[derive(Clone)]
+pub type Ctx = Arc<prisma::PrismaClient>;
 
 pub type Router = rspc::Router<Ctx>;
 
 pub(crate) fn new() -> RouterBuilder<Ctx> {
-    Router::new()
-        .config(
-            Config::new()
-                // TODO: This messes with Tauri's hot reload so we can't use it until their is a solution upstream. https://github.com/tauri-apps/tauri/issues/4617
-                .export_ts_bindings(
-                    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src/types/rspc/index.ts"),
-                )
-                .set_ts_bindings_header("/* eslint-disable */"),
+    let config = Config::new()
+        // TODO: This messes with Tauri's hot reload so we can't use it until their is a solution upstream. https://github.com/tauri-apps/tauri/issues/4617
+        .export_ts_bindings(
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../src/types/rspc/index.ts"),
         )
+        .set_ts_bindings_header("/* eslint-disable */");
+
+    Router::new()
+        .config(config)
+        // .query("version", |t| t(|ctx, _: ()| async move {
+        //     ctx.db.
+        // }))
         .query("version", |t| t(|_, _: ()| "teste"))
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,16 +3,18 @@
     windows_subsystem = "windows"
 )]
 
-use crate::api::Ctx;
+use std::{borrow::Borrow, sync::Arc};
 
 mod api;
+pub mod prisma;
 
 #[tokio::main]
 async fn main() {
     let router = api::new().build().arced();
+    let client = Arc::new(prisma::new_client().await.unwrap());
 
     tauri::Builder::default()
-        .plugin(rspc::integrations::tauri::plugin(router, || Ctx {}))
+        .plugin(rspc::integrations::tauri::plugin(router, || client))
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
